b878777f2ed71ef6c4eb4a252700855c0a2469db
Noman <nomankhialb@gmail.com>
2025-05-20 09:41:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const image = document.getElementById("IMG")
-const IMGBg = document.getElementById("IMG-bg")
-const swipeBtn = document.getElementById("swipe-btn")
+const image = document.getElementById("IMG");
+const IMGBg = document.getElementById("IMG-bg");
+const swipeBtn = document.getElementById("swipe-btn");
 
-const images = [ "./assets/woman-3567600_1920.jpg", "./assets/woman-3083453_1920.jpg", "./assets/woman-7203801_1920.jpg", "./assets/woman-8378634_1920.jpg", "./assets/woman-9264738_1920.jpg", "./assets/young-woman-4266712_1920.jpg"];
+const images = [
+  "./assets/woman-3567600_1920.jpg",
+  "./assets/woman-3083453_1920.jpg",
+  "./assets/woman-7203801_1920.jpg",
+  "./assets/woman-8378634_1920.jpg",
+  "./assets/woman-9264738_1920.jpg",
+  "./assets/young-woman-4266712_1920.jpg"
+];
 
+let track = 0;
 
-function swiper() {
-
-  const random = Math.floor(Math.random() * images.length)
-  image.src = images[random]
-  IMGBg.src = images[random]
-
-}
-
-setInterval(() => {
-  swiper()
-}, 1000);
-
-
-let track = 0
 function changeSlide() {
-  image.src = images[track % images.length]
-  IMGBg.src = images[track % images.length]
-  track += 1
+  const currentImage = images[track % images.length];
+  image.src = currentImage;
+  IMGBg.src = currentImage;
+  track++;
 }
 
-document.addEventListener("click", changeSlide)
+// Auto change every 2 seconds
+const autoSlide = setInterval(changeSlide, 2000);
+
+// Optional: also change image when user clicks the button (or anywhere)
+document.addEventListener("click", changeSlide);
+
+
